ffdd37280488950c4011646a3be8e3ccb4107bc7
Henrique-G15 <henriquegodoy1215@gmail.com>
2026-03-23 22:36:57 -0300

    Ciraçao da def gerarAssembly
    Testando read_arquivo e gerarAssembly

--- gerarAssembly.py
+++ gerarAssembly.py
@@ -3,7 +3,7 @@
 
 class gerarAssembly:
     def __init__(self):
-        pass
+        self.codigoAssembly = ""
 
     
     def read_arquivo(self, arquivo):
@@ -24,11 +24,31 @@ class gerarAssembly:
         except Exception as e:
             print(f"ERRO desconhecido ao ler o arquivo: {e}")
             return []
+    
+    def gerarAssembly(self, tokens):
+
+        """
+        Gera o assembly a partir de uma lista de tokens
+        """
+
+        for token in tokens:
+            if token.isdigit(): 
+                self.codigoAssembly += f"LOAD R0, #{token}\n"  #Carrega o numero em R0
+
+            elif token in ['+', '-', '*', '/']:  # Se o token for operador
+                self.codigoAssembly += f"{token.upper()} R1, R0\n"  #Ex simplificado
+
+            else:
+                print(f"ERRO: Token invalido {token}")
  
-# Testando read_arquivo
+        return self.codigoAssembly
+ 
+#Testando read_arquivo e gerarAssembly
 if __name__ == "__main__":
     gerador = gerarAssembly()
     linhas = gerador.read_arquivo('teste_lexico.txt') 
     print("Linhas do arquivo:")
     for linha in linhas:
-        print(linha)
+        tokens = linha.replace('(', '').replace(')', '').split()  #simplificado
+        assembly = gerador.gerarAssembly(tokens)
+        print(f"Assembly gerado para a linha: '{linha}':\n{assembly}\n")
