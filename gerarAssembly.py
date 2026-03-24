cc6e05e4c08380e377bf7c0ad9c2511dc14c8a4a
Henrique-G15 <henriquegodoy1215@gmail.com>
2026-03-24 19:49:53 -0300

    Update gerarAssembly.py
    Tratamento para suporte de decimais e  comando MEM e RES tratados diretamente

--- gerarAssembly.py
+++ gerarAssembly.py
@@ -5,20 +5,14 @@ class gerarAssembly:
     def __init__(self):
         self.codigoAssembly = ""
 
-    
     def read_arquivo(self, arquivo):
-
-        """
-        Le as linhas do arquivo e retorna uma lista de linhas
-        """
-
         try:
             with open(arquivo, 'r', encoding='utf-8') as f:
                 linhas = f.readlines()
-            return [linha.strip() for linha in linhas if linha.strip()]  #Ignora as linhas vazias
+            return [linha.strip() for linha in linhas if linha.strip()]
         
         except FileNotFoundError:
-            print(f"ERRO: O arquivo '{arquivo}' não foi encontrado")
+            print(f"ERRO: '{arquivo}' não encontrado")
             return []
         
         except Exception as e:
@@ -27,28 +21,31 @@ class gerarAssembly:
     
     def gerarAssembly(self, tokens):
 
-        """
-        Gera o assembly a partir de uma lista de tokens
-        """
-
         for token in tokens:
-            if token.isdigit(): 
-                self.codigoAssembly += f"LOAD R0, #{token}\n"  #Carrega o numero em R0
+            #SUporta numeros decimais e carrega o numero em R0
+            if token.replace('.', '', 1).isdigit():
+                self.codigoAssembly += f"LOAD R0, #{token}\n"
 
-            elif token in ['+', '-', '*', '/']:  # Se o token for operador
-                self.codigoAssembly += f"{token.upper()} R1, R0\n"  #Ex simplificado
+            elif token in ['+', '-', '*', '/']:
+                #Tem que ter pelo menos 2 operandos
+                self.codigoAssembly += f"{token.upper()} R1, R0\n"
+
+            elif token == "MEM":
+                self.codigoAssembly += "LOAD R1, MEM\n"  #Carrega da memoria
+
+            elif token == "RES":
+                self.codigoAssembly += "LOAD R1, RES\n"  #Carrega do RES
 
             else:
                 print(f"ERRO: Token invalido {token}")
- 
         return self.codigoAssembly
- 
-#Testando read_arquivo e gerarAssembly
+
+# Testando read_arquivo e gerarAssembly
 if __name__ == "__main__":
     gerador = gerarAssembly()
     linhas = gerador.read_arquivo('teste_lexico.txt') 
     print("Linhas do arquivo:")
     for linha in linhas:
-        tokens = linha.replace('(', '').replace(')', '').split()  #simplificado
+        tokens = linha.replace('(', '').replace(')', '').split()
         assembly = gerador.gerarAssembly(tokens)
-        print(f"Assembly gerado para a linha: '{linha}':\n{assembly}\n")
+        print(f"Assembly gerado da linha: '{linha}':\n{assembly}\n")
