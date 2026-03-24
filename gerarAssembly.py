e5f912b584ba761514faf80fd334edf1e988e1ac
Henrique-G15 <henriquegodoy1215@gmail.com>
2026-03-23 21:47:39 -0300

    Update gerarAssembly.py
    Testando a função read_arquivo

--- gerarAssembly.py
+++ gerarAssembly.py
@@ -1,6 +1,34 @@
 # Aluno 3 - Henrique de O. Godoy
 # Função gerarAssembly e Leitura de Arquivo
 
-def gerarAssembly():
-    # TODO: Função gerarAssembly e Leitura de Arquivo
-    pass
+class gerarAssembly:
+    def __init__(self):
+        pass
+
+    
+    def read_arquivo(self, arquivo):
+
+        """
+        Le as linhas do arquivo e retorna uma lista de linhas
+        """
+
+        try:
+            with open(arquivo, 'r', encoding='utf-8') as f:
+                linhas = f.readlines()
+            return [linha.strip() for linha in linhas if linha.strip()]  #Ignora as linhas vazias
+        
+        except FileNotFoundError:
+            print(f"ERRO: O arquivo '{arquivo}' não foi encontrado")
+            return []
+        
+        except Exception as e:
+            print(f"ERRO desconhecido ao ler o arquivo: {e}")
+            return []
+ 
+# Testando read_arquivo
+if __name__ == "__main__":
+    gerador = gerarAssembly()
+    linhas = gerador.read_arquivo('teste_lexico.txt') 
+    print("Linhas do arquivo:")
+    for linha in linhas:
+        print(linha)
